14b51e7ef1c78f6f46de0a2631169ea4953eb433
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-16 09:11:10 -0600

    Added script to draw gomuku board.

--- board.py
+++ board.py
@@ -23,18 +23,23 @@ def placeStone(xPos, yPos, player):
     boardPositions[yPos][xPos] = player
 
 pygame.init()
-screen = pygame.display.set_mode((900, 900))
+screen = pygame.display.set_mode((960, 960))
+pygame.display.set_caption("Gomuku Swap2")
 clock = pygame.time.Clock()
 running = True
 
 currentPlayer = -1
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill("purple")
+    screen.fill("peru")
+
+    for i in range(15):
+        pygame.draw.line(screen, (0, 0, 0), (60, 60+i*60), (900, 60+i*60), 5)
+    for i in range(15):
+        pygame.draw.line(screen, (0, 0, 0), (60+i*60, 60), (60+i*60, 900), 5)
 
     pygame.display.flip()
 
