6ae52a9da6c5592ba2596e62ea9b98731f6fc93b
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-16 16:50:43 -0600

    Changed code so that project can find new icon at assets/

--- board.py
+++ board.py
@@ -28,7 +28,7 @@ res = 45 # Change this according to screen resolution
 pygame.init()
 screen = pygame.display.set_mode((res*16, res*16))
 pygame.display.set_caption("Gomuku Swap2")
-img = pygame.image.load("gomuku-swap2icon.bmp")
+img = pygame.image.load("assets/gomuku-swap2icon.bmp")
 pygame.display.set_icon(img)
 clock = pygame.time.Clock()
 running = True
