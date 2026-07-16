b63b927242a8fa9aa68ecc5302c01ca9cd041770
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-15 19:19:26 -0600

    Added basic game loop for placing stones.

--- board.py
+++ board.py
@@ -18,9 +18,29 @@ boardPositions = [
 
 def placeStone(xPos, yPos, player):
     global boardPositions
-    if player != -1 and player != 1:
-        return "Invalid player"
-    if boardPositions[yPos][xPos] == 0:
-        boardPositions[yPos][xPos] = player
-    else:
-        return "Position occupied"
+    boardPositions[yPos][xPos] = player
+    return boardPositions
+
+running = True
+currentPlayer = -1
+while running:
+    inputValid = False
+    while not inputValid:
+        try:
+            xPos = int(input("Enter xPos"))
+            yPos = int(input("Enter yPos"))
+        except:
+            print("Input invalid")
+        if xPos < 0 or xPos > 14:
+            print("Invalid position")
+            break
+        if yPos < 0 or yPos > 14:
+            print("Invalid position")
+            break
+        if boardPositions[yPos][xPos] != 0:
+             print("Position occupied")
+             break
+        inputValid = True
+
+    print(placeStone(xPos, yPos, currentPlayer))
+    currentPlayer *= -1
