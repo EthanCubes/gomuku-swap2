f1abed521219757b843cb69be42bda5720f22d44
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-17 15:49:39 -0600

    Changed varables to be global so variables work across Python files.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
 import pygame
+import globals as g
 
 res = 45
 
 pygame.init()
-screen = pygame.display.set_mode((res*16, res*16))
+g.screen = pygame.display.set_mode((res*16, res*16))
 pygame.display.set_caption("Gomuku Swap2")
-img = pygame.image.load("assets/gomuku-swap2icon.bmp")
-pygame.display.set_icon(img)
-clock = pygame.time.Clock()
-running = True
+g.img = pygame.image.load("assets/gomuku-swap2icon.bmp")
+pygame.display.set_icon(g.img)
+g.clock = pygame.time.Clock()
+g.running = True 
 
 mode = "mainMenu"
 
-while running:
+while g.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            g.running = False
     try:
         pygame.display.flip()
     except:
