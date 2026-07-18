11c8dc6e78a97c3bf323bea305ba49c9c4094ef0
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-18 10:06:10 -0600

    Added option for main python file to draw a menu by means of another python file.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
 import pygame
 import globals as g
 
-res = 45
+from mainMenu import mainMenu
 
 pygame.init()
-g.screen = pygame.display.set_mode((res*16, res*16))
+g.screen = pygame.display.set_mode((g.res*16, g.res*16))
 pygame.display.set_caption("Gomuku Swap2")
 g.img = pygame.image.load("assets/gomuku-swap2icon.bmp")
 pygame.display.set_icon(g.img)
 g.clock = pygame.time.Clock()
 g.running = True 
 
-mode = "mainMenu"
+g.mode = "mainMenu"
 
 while g.running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             g.running = False
+    mainMenu()
     try:
         pygame.display.flip()
     except:
