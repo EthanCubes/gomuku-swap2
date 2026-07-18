80d8a6fe3e704a5fc3715e6c2070a4bd0cfbc772
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-18 13:31:33 -0600

    Changed code to center play button

--- src/mainMenu.py
+++ src/mainMenu.py
@@ -5,7 +5,7 @@ image = pygame.image.load("assets/playWFriends.bmp")
 
 def mainMenu():
     g.screen.fill("peru")
-    g.screen.blit(image, (g.res*8, g.res*8))
-    buttonClicked = g.buttonClicked((g.res*6, g.res*7), (g.res*4, g.res*2))
+    g.screen.blit(image, (g.res*8-50, g.res*8-20))
+    buttonClicked = g.buttonClicked((g.res*8-50, g.res*8-20), (g.res*8+50, g.res*8+20))
     if buttonClicked:
         g.mode = "playing"
