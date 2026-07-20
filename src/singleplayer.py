87feb2cf8a64ebb9faef8231b5b6fc4d8b434e83
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-19 20:13:21 -0600

    Removed unnecessary debug print statement.

--- src/singleplayer.py
+++ src/singleplayer.py
@@ -8,10 +8,6 @@ import bot
 
 def setup():
     g.starter = randint(0, 1) # 0 is bot, 1 is human
-    if g.starter == 0:
-        print("You are black")
-    else:
-        print("You are white")
 
 def gameloop():
     if g.currentPlayer == 1:
