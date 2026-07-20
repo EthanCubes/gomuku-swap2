ace2c8947bddacb1166b4e7bb0480601290d5529
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-20 17:53:38 -0600

    Changed random place by bot into separate function.

--- src/bot.py
+++ src/bot.py
@@ -3,21 +3,25 @@ from random import randint
 import board as b
 from time import sleep
 
-def analyze():
-    # check for 4 in a row to block, split 2 in a row
-    
-    # check for ways to get 4 in a row in 1 turn, split 4 in a row
-    # check for unblocked 3 in a row to block, split 3 in a row
-    # check for ways to get unblocked 3 in a row in 1 turn, split 3 in a row
-    # check for ways to make 2x2 square
-    pass
-
-def botPlaceStone():
-    sleep(0.25)
+def placeRandomly():
     valid = False
     while not valid:
         pos1 = randint(0, 14)
         pos2 = randint(0, 14)
         if g.boardPositions[pos2][pos1] == 0:
             valid = True
+    return pos1, pos2
+
+def analyze():
+    '''
+    ***Roadmap***
+    Scan for immediate danger/win
+    Place to create threats/thwart ones
+    Place randomly because why not
+    '''
+    return placeRandomly()
+
+def botPlaceStone():
+    sleep(0.25)
+    pos1, pos2 = analyze()
     b.placeStone(pos1, pos2, g.currentPlayer)
