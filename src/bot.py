a6b3f802ab33835711f7ba44065fca7395af3d62
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-20 20:15:32 -0600

    Added code that makes bot attempt to block user when the user has 4 in a row.

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
 import globalData as g
 from random import randint
 import board as b
+import win as w
+import copy
 
 def placeRandomly():
     valid = False
@@ -18,8 +20,26 @@ def analyze():
     Place to create threats/thwart ones
     Place randomly because why not
     '''
+    testBoard = copy.deepcopy(g.boardPositions)
+    for x in range(15):
+        for y in range(15):
+            if testBoard[y][x] == 0:
+                testBoard[y][x] = g.botColor
+                if w.generateScan(testBoard) == g.botColor:
+                    return x,y
+                testBoard[y][x] = 0
+    for x in range(15):
+        for y in range(15):
+            if testBoard[y][x] == 0:
+                testBoard[y][x] = g.playerColor
+                if w.generateScan(testBoard) == g.playerColor:
+                    return x,y
+                testBoard[y][x] = 0
     return placeRandomly()
+                
+
 
 def botPlaceStone():
     pos1, pos2 = analyze()
+    print(pos1, pos2)
     b.placeStone(pos1, pos2, g.currentPlayer)
