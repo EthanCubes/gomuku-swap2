f47dc9e62b5e2acb56c8344d2f03f72c7ef86fda
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-22 22:43:36 -0600

    (Temporary Change) Removed scanning code to get a fresh start when scanning code is optimized.

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ from random import randint
 import board as b
 import win as w
 import copy
-import massiveChunksOfCopypastaCode as mcocc
+import scan as sc
 
 def placeRandomly():
     valid = False
@@ -21,56 +21,6 @@ def analyze():
     Place to create threats/thwart ones
     Place randomly because why not
     '''
-    testBoard = copy.deepcopy(g.boardPositions)
-    # Check for ways to win in 1 turn
-    for x in range(15):
-        for y in range(15):
-            if testBoard[y][x] == 0:
-                testBoard[y][x] = g.botColor
-                if w.generateScan(testBoard)[1] == g.botColor:
-                    return x,y
-                testBoard[y][x] = 0
-    # Check for 4-in-a-rows to block
-    for x in range(15):
-        for y in range(15):
-            if testBoard[y][x] == 0:
-                testBoard[y][x] = g.playerColor
-                if w.generateScan(testBoard)[1] == g.playerColor:
-                    return x,y
-                testBoard[y][x] = 0
-    # Check for ways to get 4-in-a-row in one turn
-    for x in range(15):
-        for y in range(15):
-            if testBoard[y][x] == 0:
-                testBoard[y][x] = g.botColor
-                scan = mcocc.generateScan4(testBoard)
-                if scan[0] == g.botColor:
-                    if scan[1] == (x,y) or scan[2] == (x,y) or scan[3] == (x,y) or scan[4] == (x,y):
-                        return x,y
-                testBoard[y][x] = 0
-    # Check for any way to block 3-in a rows
-    for x in range(15):
-        for y in range(15):
-            if testBoard[y][x] == 0:
-                testBoard[y][x] = g.playerColor
-                scan = mcocc.generateScan4(testBoard)
-                if scan[0] == g.playerColor:
-                    if scan[1] == (x,y) or scan[2] == (x,y) or scan[3] == (x,y) or scan[4] == (x,y):
-                        return x,y
-                testBoard[y][x] = 0
-    # Check for any way to get 3-in-a-row
-    for x in range(15):
-        for y in range(15):
-            if testBoard[y][x] == 0:
-                testBoard[y][x] = g.botColor
-                scan = mcocc.generateScan3(testBoard)
-                if scan[0] == g.botColor:
-                    if scan[1] == (x,y) or scan[2] == (x,y) or scan[3] == (x,y):
-                        return x,y
-                testBoard[y][x] = 0
-    if randint(1,100) > 25:
-        return mcocc.connect2()
-    return placeRandomly()
                 
 
 
