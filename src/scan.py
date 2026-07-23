765341784a7e15bc0acef0801b57a908172cbdd1
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-22 22:51:35 -0600

    Added the basic framework for the scan generation process.

--- src/scan.py
+++ src/scan.py
@@ -1,6 +1,18 @@
 import random
 import globalData as g
 
+def scanBoard(x, y, pattern):
+    pass
+
+def generateScan(pattern):
+    posList = []
+    for y in range(15):
+        for x in range(15):
+            scan = scanBoard(x,y,pattern)
+            if scan == -1 or scan == 1:
+                posList.append((scan, (x, y))) # Appends to the list color of the combonation and the coordinates. 
+    return posList
+
 # This function is like pretty good for now, doesn't need to be changed.
 def connect2():
     import globalData as g
@@ -15,4 +27,3 @@ def connect2():
             x += dlist[direction][0]
             if g.boardPositions[y][x] == 0:
                 return x, y
-generateScan([0,0,0,0,0,0])
