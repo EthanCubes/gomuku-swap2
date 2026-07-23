449a13d7c22f9e8ea27f018d6cb534fb7834a367
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-23 16:55:03 -0600

    Changed scan algorithm to not return when empty

--- src/scan.py
+++ src/scan.py
@@ -26,26 +26,29 @@ def scanBoard(x, y, pattern): # now this is the hard part.
     return None
 
 def generateScan(pattern, color): # pattern has to be 6 characters.
+    items = 0
     posList = []
     for y in range(15):
         for x in range(15):
             scan = scanBoard(x,y,pattern)
             if scan == -1 or scan == 1:
+                items += 1
                 posList.append((scan, (x, y))) # Appends to the list color of the combonation and the coordinates.
     whitelist = []
     blacklist = []
-    if color != 0:
-        for item in posList:
-            if item[0] == 1:
-                whitelist.append(item)
-            else:
-                blacklist.append(item)
-    else:
-        return posList
-    if color == 1:
-        return whitelist
-    elif color == -1:
-        return blacklist
+    if items > 0:
+        if color != 0:
+            for item in posList:
+                if item[0] == 1:
+                    whitelist.append(item)
+                else:
+                    blacklist.append(item)
+        else:
+            return posList
+        if color == 1:
+            return whitelist
+        elif color == -1:
+            return blacklist
 
 # This function is like pretty good for now, doesn't need to be changed.
 def connect2():
