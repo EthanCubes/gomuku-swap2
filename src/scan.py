86d1aca57e7e646d73eb012a9f09b2110775f55f
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-23 12:36:58 -0600

    Added basic code for scanning. I have yet to find a way to convert this into something actually usable though.

--- src/scan.py
+++ src/scan.py
@@ -1,16 +1,38 @@
 import random
 import globalData as g
 
-def scanBoard(x, y, pattern):
-    pass
+def scanBoard(x, y, pattern): # now this is the hard part.
+    reverse = []
+    for item in pattern:
+        reverse.append(-item)
+    dlist = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)] # set up scanning in all 8 directions
+    scanList = []
+    # Gets the status and puts it in a list
+    for direction in dlist:
+        dirScanList = [] # directional scan
+        for i in range(6):
+            dirScanList.append((x+i*int(direction[0]), y+i*int(direction[1])))
+        statusList = []
+        for item in dirScanList:
+            print(item)
+            if not (item[0] < 0 or item[0] > 14 or item[1] < 0 or item[1] > 14):
+                statusList.append(g.boardPositions[item[1]][item[0]])
+        scanList.append(statusList)
+    # Compares the list to the given pattern
+    for item in scanList:
+        if item == pattern:
+            return 1
+        elif item == reverse:
+            return -1
+    return None
 
-def generateScan(pattern):
+def generateScan(pattern): # pattern has to be 6 characters.
     posList = []
     for y in range(15):
         for x in range(15):
             scan = scanBoard(x,y,pattern)
             if scan == -1 or scan == 1:
-                posList.append((scan, (x, y))) # Appends to the list color of the combonation and the coordinates. 
+                posList.append((scan, (x, y))) # Appends to the list color of the combonation and the coordinates.
     return posList
 
 # This function is like pretty good for now, doesn't need to be changed.
