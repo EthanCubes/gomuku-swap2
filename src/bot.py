0d2c3ffdf0362c9f623c64216ea3769a05b3b51e
EthanCubes <ethanshangyilin@protonmail.com>
2026-07-23 17:14:29 -0600

    Cleaned up code so that it hopefully works now.

--- src/bot.py
+++ src/bot.py
@@ -35,17 +35,16 @@ def analyze():
     # Get 5
     scanned.append(sc.generateScan([0,1,1,1,1,0], g.botColor))
     scanned.append(sc.generateScan([0,1,1,1,1,-1], g.botColor))
-    print(sc.generateScan([0,1,1,1,1,-1], g.botColor))
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
     scanned.clear()
     # Block closed 4
     scanned.append(sc.generateScan([0,1,1,1,1,-1], g.playerColor))
-    scanned.clear()
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
+    scanned.clear()
     # Get closed/open 4
     scanned.append(sc.generateScan([0,1,1,1,0,0], g.botColor))
     scanned.append(sc.generateScan([0,1,1,1,0,-1], g.botColor))
@@ -53,14 +52,14 @@ def analyze():
     scanned.append(sc.generateScan([0,1,1,1,-1,1], g.botColor))
     scanned.append(sc.generateScan([0,1,1,1,-1,-1], g.botColor))
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
     scanned.clear()
     # Block open 3
     scanned.append(sc.generateScan([0,1,1,1,0,0], g.playerColor))
     scanned.append(sc.generateScan([0,1,1,1,0,-1], g.playerColor))
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
     scanned.clear()
     # Get Open 3
@@ -74,7 +73,8 @@ def analyze():
     scanned.append(sc.generateScan([0,1,1,0,-1,1], g.botColor))
     scanned.append(sc.generateScan([0,1,1,0,-1,-1], g.botColor))
     for item in scanned:
-        if scanned != []:
+        print(item)
+        if item != None:
             return item[1][0], item[1][1]
     scanned.clear()
     # Block closed 3
@@ -82,9 +82,8 @@ def analyze():
     scanned.append(sc.generateScan([0,1,1,1,-1,1], g.playerColor))
     scanned.append(sc.generateScan([0,1,1,1,-1,-1], g.playerColor))
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
-    scanned.clear()
     # Get closed 3
     scanned.append(sc.generateScan([0,1,1,-1,0,0], g.botColor))
     scanned.append(sc.generateScan([0,1,1,-1,0,1], g.botColor))
@@ -96,18 +95,18 @@ def analyze():
     scanned.append(sc.generateScan([0,1,1,-1,-1,1], g.botColor))
     scanned.append(sc.generateScan([0,1,1,-1,-1,-1], g.botColor))
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[1][0], item[1][1]
     scanned.clear()
     # Get 2 in a row
     scanned.append(sc.connect2())
     for item in scanned:
-        if scanned != []:
+        if item != None:
             return item[0], item[1]
     scanned.clear()
     # Place randomly
     return placeRandomly()
 
 def botPlaceStone():
-    analyze()
-    # b.placeStone(pos1, pos2, g.currentPlayer)
+    pos1, pos2 = analyze()
+    b.placeStone(pos1, pos2, g.currentPlayer)
